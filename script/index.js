4753ba763eaf034a57a3e950c9419fdafe6dc4e7
max <malalamobilelegends@gmail.com>
2023-08-01 16:06:31 +1000

    ''

---
+++ script/index.js
@@ -0,0 +1,13 @@
+// Заголовок модального окна
+const modalTitle = document.querySelector(".modal__title");
+
+// Форма внутри модального окна
+const modalForm = document.querySelector(".modal__form");
+
+// Чекбокс
+const checkbox = document.querySelector(".modal__checkbox");
+
+// Поле рядом с чекбоксом
+const fieldNearCheckbox = document.querySelector(".modal__input_discount");
+
+console.log(modalTitle);
