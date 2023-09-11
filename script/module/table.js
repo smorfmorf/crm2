a64f9679c7ad086c1402988accb93e1769c74118
max <malalamobilelegends@gmail.com>
2023-09-12 00:12:43 +1000

    ''

--- script/module/table.js
+++ script/module/table.js
@@ -20,7 +20,7 @@ function createRow(obj) {
     <td class="table__cell">${obj.price}</td>
     <td class="table__cell">${obj.count * obj.price}</td>
     <td class="table__cell table__cell_btn-wrapper">
-      <button class="table__btn table__btn_pic"></button>
+      <button class="table__btn table__btn_pic" data-pic="../../img/forest.jpg"></button>
       <button class="table__btn table__btn_edit"></button>
       <button class="table__btn table__btn_del"></button>
     </td>
@@ -41,8 +41,34 @@ function renderGoodsTable() {
   });
 }
 
+//*Открываем новое окно с картинкой
+function openImageInNewWindow(event) {
+  const target = event.target;
+  const imageUrl = target.getAttribute("data-pic");
+
+  if (imageUrl) {
+    const windowHeight = 600; // Высота окна
+    const windowWidth = 800; // Ширина окна
+    const top = (screen.height - windowHeight) / 2;
+    const left = (screen.width - windowWidth) / 2;
+
+    // Открываем новое окно браузера
+    window.open(
+      imageUrl,
+      "_blank",
+      `width=${windowWidth}, height=${windowHeight}, top=${top}, left=${left}`
+    );
+  }
+}
+
 function initTable() {
   renderGoodsTable();
+
+  // Найти все кнопки с классом table_btn_pic и добавить обработчик события на клик
+  const picButtons = document.querySelectorAll(".table__btn_pic");
+  picButtons.forEach((button) => {
+    button.addEventListener("click", openImageInNewWindow);
+  });
 
   //! Добавляем новый слушатель
   cms.addEventListener("click", handleDeleteButtonClick);
